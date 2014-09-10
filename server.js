08e04988b5ef6403b6ae16a008aec8fc5c93c534
Mandi Grant <mandi018@gmail.com>
2014-09-09 21:03:32 -0700

    Adding a simple server.js file

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var http = require('http');
+var app = express();
+
+app.use(express.static(__dirname + (process.env.STATIC_DIR || '/build')));
+
+app.use(bodyparser.json());
+
+var server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000, function() {
+  console.log('server running on port 3000');
+});
