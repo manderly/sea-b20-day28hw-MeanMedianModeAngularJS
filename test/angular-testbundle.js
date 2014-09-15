b40eb3252998b90dd0ca907491876c860b410f62
Mandi Grant <mandi018@gmail.com>
2014-09-15 10:31:55 -0700

    Finished math service tests

--- test/angular-testbundle.js
+++ test/angular-testbundle.js
@@ -25294,9 +25294,24 @@ describe('Mean Median Mode service', function() {
 
   beforeEach(angular.mock.module('mmmApp'));
 
+  it('should have a working math service', inject(function(mathService) {
+    expect(mathService).toBeDefined();
+  }));
 
-  it('should find the average', inject(function(mathService) {
+  it('should find the mean', inject(function(mathService) {
     expect(mathService.getMean("1,2,3")).toBe(2);
+  }));
+
+  it('should find the median', inject(function(mathService){
+    expect(mathService.getMedian("1,2,3")).toBe(2);
+  }));
+
+  it('should find the mode where there is a mode', inject(function(mathService) {
+    expect(mathService.getMode("1,2,2,3")).toBe(2);
+  }));
+
+  it('should find NO MODE where there is no mode', inject(function(mathService) {
+    expect(mathService.getMode("1,2,3")).toBe("NO MODE");
   }));
 
 });
